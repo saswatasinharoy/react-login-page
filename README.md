25de8f42e2b92fcff93651ca863ee0202fb8833b
izengod <tathagata.sinharoy91@gmail.com>
2020-09-24 19:10:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sign-up page: Saswata Sinha Roy
+
+In the project directory, you can run:
+
+### `npm install`
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './components/signup/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
